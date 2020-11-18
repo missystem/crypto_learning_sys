d86adce25ccd54c47c60709163b97307ca1a267a
Yuhan Shi <yuhans@protonmail.com>
2020-11-17 23:39:17 -0800

    add tests

--- temptest.py
+++ temptest.py
@@ -16,28 +16,36 @@ from prime_list import *
 
 def main():
     # generate 20 prime numbers
-    num_prime = 2
-    prime_list = randomNumGenerator(num_prime)
-    p = random.choice(prime_list)
-    prime_list.remove(p)
-    q = random.choice(prime_list)
-    prime_list.remove(q)
-    print(f"random item from list is: {p}, and {q}")
-
-    # rsa_result = rsa(233, 2333, 97, 345)
-    e = random.randint(1, 1000)
-    print(f"number e is {e}")
-    message = random.randint(100, 1000)
-    print(f"message is {message}")
+    # num_prime = 2
+    # prime_list = randomNumGenerator(num_prime)
+    # p = random.choice(prime_list)
+    # prime_list.remove(p)
+    # q = random.choice(prime_list)
+    # prime_list.remove(q)
+    # print(f"random item from list is: {p}, and {q}")
+    #
+    # # rsa_result = rsa(233, 2333, 97, 345)
+    # e = random.randint(1, 1000)
+    # print(f"number e is {e}")
+    # message = random.randint(100, 1000)
+    # print(f"message is {message}")
+    print("-" * 100)
+    p = 23
+    q = 31
+    e = 61
+    message = 100
     rsa_result = rsa(p, q, e, message)
-    print("[p, q, N, r, e, publicKey, message, encryptedMsg, d, privateKey, decryptedMsg]")
+    print("[p, q, N, phi, e, publicKey, message, encryptedMsg, d, privateKey, decryptedMsg]")
     print(rsa_result)
+    print("-" * 100)
     print()
     # ['233', '2333', '543589', '541024', '97', '(97, 543589)', '345', '451737', '373697', '(373697, 543589)', '345']
 
+    print("=" * 100)
     print("[p, g, a, b, A, B, A', B']")
     dh_result = diffie_hellman(2333, 233, 97, 345)
     print(dh_result)
+    print("=" * 100)
     print()
     # ['2333', '233', '97', '345', '1613', '357', '93', '93']
 
@@ -47,6 +55,8 @@ def main():
     # ['23333', '233', '776', '19729', '456', '345', '(16065, 19636)', '345']
     print()
 
+
+    print("-------------------------")
     print("[message, generatedKey, encryptedMsg, decryptedMsg]")
     message = "Hi! How are you? I am good, thank you."
     sub_result = substitution(message)
