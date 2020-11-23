e1c34e8ab7bcd668e762d5884c4fecf6784a789f
Yuhan Shi <yuhans@protonmail.com>
2020-11-23 03:35:22 -0800

    add two new functions return prime number list

--- crypto_implementation/prime_list.py
+++ crypto_implementation/prime_list.py
@@ -11,6 +11,26 @@ import math
 import random
 
 
+def prime_num(n: int) -> list:
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if sieve[i]:
+            sieve[i * i::2 * i] = [False] * ((n - i * i - 1) // (2 * i) + 1)
+    return [2] + [i for i in range(3, n, 2) if sieve[i]]
+
+
+def primesInRange(n):
+    """return a list of prime numbers smaller than n"""
+    fact = 1
+    primes = []
+    for k in range(2, n):
+        fact = fact * (k - 1)
+        if (fact + 1) % k == 0:
+            primes.append(k)
+    return primes
+
+
 def fast_powering(n, pow, modulus):
     res = 1
     n = n % modulus
