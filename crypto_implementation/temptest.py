63017e263e5f5fda83fcbcfc8d6e0bdb00554fad
Yuhan Shi <yuhans@protonmail.com>
2020-11-23 04:01:17 -0800

    add testing for each cryptosystem implementation

--- crypto_implementation/temptest.py
+++ crypto_implementation/temptest.py
@@ -12,56 +12,98 @@ from DH import *
 from Elgamal import *
 from substitution import *
 from prime_list import *
+import random
+import math
 
 
-def main():
-    # generate 20 prime numbers
-    # num_prime = 2
-    # prime_list = randomNumGenerator(num_prime)
-    # p = random.choice(prime_list)
-    # prime_list.remove(p)
-    # q = random.choice(prime_list)
-    # prime_list.remove(q)
-    # print(f"random item from list is: {p}, and {q}")
-    #
-    # # rsa_result = rsa(233, 2333, 97, 345)
-    # e = random.randint(1, 1000)
+def primePQ(num_prime):
+    """generate two prime numbers """
+    prime_list = randomNumGenerator(num_prime)
+    p = random.choice(prime_list)
+    prime_list.remove(p)
+    q = random.choice(prime_list)
+    prime_list.remove(q)
+    return p, q
+
+
+def eGenerator(r):
+    """generate a valid e"""
+    e = random.randint(1, 1000)
+    while math.gcd(e, r) != 1:
+        e = random.randint(1, 1000)
+    return e
+
+
+def rsaTester():
+    print(f"{'-' * 42} RSA tester {'-' * 42}")
+    num_prime = 20
+    p, q = primePQ(num_prime)
+    # print(f"random prime p = {p}, q = {q}")
+
+    r = (p - 1) * (q - 1)
+    e = eGenerator(r)
     # print(f"number e is {e}")
-    # message = random.randint(100, 1000)
+    message = random.randint(100, 1000)
     # print(f"message is {message}")
-    print("-" * 100)
-    p = 23
-    q = 31
-    e = 61
-    message = 100
+
+    if math.gcd(e, (p - 1) * (q - 1)) != 1:
+        print("NOOOOOOOOOOOOOOOOOOOOO")
+        return
+
     rsa_result = rsa(p, q, e, message)
     print("[p, q, N, phi, e, publicKey, message, encryptedMsg, d, privateKey, decryptedMsg]")
     print(rsa_result)
-    print("-" * 100)
     print()
+    # simple example result:
     # ['233', '2333', '543589', '541024', '97', '(97, 543589)', '345', '451737', '373697', '(373697, 543589)', '345']
 
-    print("=" * 100)
+
+def dhTester():
+    print(f"{'-' * 36} Diffie Hellman tester {'-' * 36}")
     print("[p, g, a, b, A, B, A', B']")
-    dh_result = diffie_hellman(2333, 233, 97, 345)
+    p, q = primePQ(20)
+    g = random.randint(10, p)
+    a = random.randint(1, 1000)
+    b = random.randint(1, 1000)
+    dh_result = diffie_hellman(p, g, a, b)
     print(dh_result)
-    print("=" * 100)
     print()
+    # simple example result:
     # ['2333', '233', '97', '345', '1613', '357', '93', '93']
 
+
+def elgamalTester():
+    print(f"{'-' * 40} Elgamal tester {'-' * 40}")
     print("[p, g, a, publicKeyA, k, message, encryptedMsg, decryptedMsg]")
-    elgamal_result = elgamal(23333, 233, 776, 456, 345)
+    p, q = primePQ(20)
+    g = random.randint(10, 1000)
+    a = random.randint(1, p-1)
+    k = random.randint(10, 100)
+    m = random.randint(10, 1000)
+    elgamal_result = elgamal(p, g, a, k, m)
     print(elgamal_result)
-    # ['23333', '233', '776', '19729', '456', '345', '(16065, 19636)', '345']
     print()
+    # simple example result:
+    # ['23333', '233', '776', '19729', '456', '345', '(16065, 19636)', '345']
 
 
-    print("-------------------------")
+def subTester():
+    print(f"{'-' * 38} Substitution tester {'-' * 38}")
     print("[message, generatedKey, encryptedMsg, decryptedMsg]")
     message = "Hi! How are you? I am good, thank you."
     sub_result = substitution(message)
     print(sub_result)
     print()
+
+
+def main():
+    for i in range(2):
+        rsaTester()
+        dhTester()
+        elgamalTester()
+
+    # subTester()
+
 
 
 if __name__ == '__main__':
